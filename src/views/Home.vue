0c7e947380145f2186989d3039d505b3f9c7a1a6
4060ti <jack964270261@gmail.com>
2023-12-28 00:53:44 +0800

    reponse encrypt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,16 @@
     <el-form class="home-container" label-position="left" label-width="0px">
       <h3 class="home_title">
         已认证
-        <el-button @click="toGetApiTest()">GET接口测试</el-button>
+        <el-button @click="toGetApiTest()">点击获取邮件</el-button>
       </h3>
       <el-form-item label="">
         <el-input type="text" v-model="homeForm.nickName" autocomplete="off" placeholder="昵称"></el-input>
       </el-form-item>
       <el-form-item label="">
         <el-input type="text" v-model="tempPhomeNumber" autocomplete="off" placeholder="手机号"></el-input>
+      </el-form-item>
+      <el-form-item label="">
+        <el-input type="label" v-model="email" autocomplete="off" placeholder="点击获取显示邮件" readonly></el-input>
       </el-form-item>
       <!-- <el-form-item style="width: 100%;">
         <el-button type="primary" style="width: 100%;background: #505458;border: none;" @click="toLogin">登陆</el-button>
@@ -19,7 +22,7 @@
 </template>
 
 <script>
-import { encryptSM4, SM4Data } from '@/utils/encrypto/sm4'
+import { encryptSM4, decryptSM4, SM4Data } from '@/utils/encrypto/sm4'
 
 export default {
   name: 'Home',
@@ -29,7 +32,8 @@ export default {
           nickName: "",
           phoneNumber: ""
         },
-        tempPhomeNumber: ""
+        tempPhomeNumber: "",
+        email: ""
       }
     },
     methods: {
@@ -40,9 +44,13 @@ export default {
         SM4Data.originalData = this.homeForm.phoneNumber
         encryptSM4(SM4Data)
         this.homeForm.phoneNumber = SM4Data.encryptedData
+
         this.axios.get('/getApiTest', { params: this.homeForm }).then((resp) =>{
           let data = resp.data;
           if(data.code==200){
+            SM4Data.encryptedData = data.data.email
+            decryptSM4(SM4Data)
+            this.email = SM4Data.decryptedData
             this.$message({
               message: data.msg,
               type:'success'

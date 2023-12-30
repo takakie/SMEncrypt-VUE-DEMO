f04dbc785e4af8b1e27552b7cb67199e60ba34a7
4060ti <jack964270261@gmail.com>
2023-12-30 10:22:57 +0800

    version-0.1

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
@@ -16,11 +15,6 @@ const routes = [
     path: '/Login',
     name: 'login',
     component: Login
-  },
-  {
-    path: '/Register',
-    name: 'register',
-    component: Register
   },
   {
     path: '/about',
